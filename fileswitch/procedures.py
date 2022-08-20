323a0ce73949ef9cd6a6af01cd2dbb2819992dd3
Marcel Johannesmann <mj0nez@fn.de>
2022-08-20 23:48:03 +0200

    added print procedure util

--- fileswitch/procedures.py
+++ fileswitch/procedures.py
@@ -11,3 +11,9 @@ class Procedure:
 
     def __repr__(self) -> str:
         return f"<PROCEDURE: {self.__class__.__name__}>: {self.description}"
+
+
+def get_print_procedure() -> Procedure:
+    return Procedure(
+        lambda x: print(f"File {x} matches filter!"), "Prints the file name."
+    )
