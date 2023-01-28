9b02f702f899769de4b20eed309e9cb71b40eb61
Marcel Johannesmann <mj0nez@fn.de>
2023-01-28 21:14:35 +0100

    added test 4 move between same fs

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,6 +1,7 @@
-from fileswitch.operator import move_file
+# from fileswitch.operator import move_file
 from fs import open_fs
 from pathlib import Path
+from fs.move import move_file
 
 
 def test_move_between_2_fs():
@@ -13,12 +14,15 @@ def test_move_between_2_fs():
 
 
 def test_move_between_same_fs():
-    fs_data = open_fs("./tests/data")
+    fs_data = open_fs("tests/data")
     fs_data.writetext("target/README.md", "Tetris clone")
 
-    move_file(fs_data, "target/README.md", fs_data, "source/README.md", True)
-    
-    # ?!?!
-    # the test fails if the target path does not exists 
+    move_file(
+        src_fs=fs_data,
+        src_path="target/README.md",
+        dst_fs=fs_data,
+        dst_path="source/README.md",
+        preserve_time=True,
+    )
 
-    # fs_data.remove("./source/README.md")
+    fs_data.remove("source/README.md")
